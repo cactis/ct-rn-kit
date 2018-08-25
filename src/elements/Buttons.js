1ff2a52ff7c857e5472d61904f4ad59d797db171
cactis <cactis.lin@gmail.com>
2018-08-25 22:32:44 +0800

    module project setup

---
+++ src/elements/Buttons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { View, Button } from 'react-native'
+
+export default class RoundButton extends React.PureComponent {
+  render() {
+    return <View><Button title='aaa'/></View>
+  }
+}
