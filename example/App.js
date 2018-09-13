e6477707a2f9b51475308f73bf849ab520fa529e
cactis <cactis.lin@gmail.com>
2018-09-13 21:56:52 +0800

    調整 Screen6

--- example/App.js
+++ example/App.js
@@ -10,13 +10,13 @@ console.disableYellowBox = true
 export default class App extends Component<Props> {
   render() {
     return <Scroll horizontal pagingEnabled>
-      <Page><Screens.Screen11 /></Page>
-      <Page><Screens.DocScreen /></Page>
+      {/* <Page><Screens.Screen11 /></Page>
       <Page><Screens.Screen10 /></Page>
       <Page><Screens.Screen9 /></Page>
       <Page><Screens.Screen8 /></Page>
-      <Page><Screens.Screen7 /></Page>
+      <Page><Screens.Screen7 /></Page> */}
       <Page><Screens.Screen6 /></Page>
+      <Page><Screens.DocScreen /></Page>
       <Page><Screens.Screen5 /></Page>
       <Page><Screens.Screen4 /></Page>
       <Page><Screens.Screen3 /></Page>
--- example/src/screens/Screen6.js
+++ example/src/screens/Screen6.js
@@ -4,33 +4,36 @@ import * as CT from '../../..'
 import { BaseScreen } from './'
 
 export class Screen6 extends Component {
+  state = {
+    uri: 'https://images.pexels.com/photos/775199/pexels-photo-775199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  }
   render() {
     return <BaseScreen number={6}>
-      <CT.ImageBackground uri='https://images.pexels.com/photos/1298684/pexels-photo-1298684.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'>
-      <CT.Row><CT.Space/></CT.Row>
-      <CT.Row padding={20}>
-        <CT.Text theme='H1' color='white' numberOfLines={2} size={40}>{"Out of \nthe Wild Ocean"}</CT.Text>
-        <CT.Row>
-          <CT.Stars value={4.5} size={18} />
+      <CT.ImageBackground
+        uri={this.state.uri}>
+        <CT.Row><CT.Space/></CT.Row>
+        <CT.Row padding={20}>
+          <CT.Text theme='H1' color='white' style={{fontWeight: '900', fontFamily: 'HoeflerText-Regular'}} numberOfLines={2} size={40}>{"Out of \nthe Wild Forest"}</CT.Text>
+          <CT.Row>
+            <CT.Stars value={4.5} size={12} />
+          </CT.Row>
+          <CT.Row height={100} justifyContent='space-evenly' layout='row'>
+            <Info key={1} text='2h' label='Tourtime' icon='clock-o' />
+            <Info key={2} text='$10' label='Prie' icon='ios-pricetag' iconSet='Ionicons' />
+            <Info key={3} text='4' label='Person' icon='md-people' iconSet='Ionicons' />
+            <Info key={4} text='15h' label='Close' icon='bell-sleep' iconSet='MaterialCommunityIcons' />
+          </CT.Row>
+          <CT.Row justifyContent='flex-end' marginBottom={20}>
+            <CT.Button theme='pill' backgroundColor='rgba(139,179,215,.4)'  color='white' title='See Details' />
+          </CT.Row>
         </CT.Row>
-        <CT.Row height={100} justifyContent='space-evenly' layout='row'>
-          <Info key={1} text='2h' label='Tourtime' icon='clock-o' />
-          <Info key={2} text='$10' label='Prie' icon='ios-pricetag' iconSet='Ionicons' />
-          <Info key={3} text='4' label='Person' icon='md-people' iconSet='Ionicons' />
-          <Info key={4} text='15h' label='Close' icon='bell-sleep' iconSet='MaterialCommunityIcons' />
-        </CT.Row>
-        <CT.Row justifyContent='flex-end' marginBottom={20}>
-          <CT.Button theme='pill' backgroundColor='rgba(139,179,215,.4)' color='white' title='See Details' />
-        </CT.Row>
-      </CT.Row>
-    </CT.ImageBackground>
-  </BaseScreen>
+      </CT.ImageBackground>
+    </BaseScreen>
+  }
 }
-}
-
 const Info = (props) => {
   return <CT.Div alignItems='center' layout='column'>
-    <CT.Text theme='H5' color='white' style={{textAlign: 'center'}}>{props.text}</CT.Text>
+    <CT.Text theme='H1' color='white' style={{textAlign: 'center'}}>{props.text}</CT.Text>
     <CT.IconLabel padding={1}
       icon={<CT.Icon name={props.icon} color='white' size={12} {...props}/>}
       label={<CT.Label theme='H8' color='white'>{props.label}</CT.Label>}
